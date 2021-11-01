f6f3fa2ff537bf2f619f362375c97aa2d5fecdb2
Ruslan <kornerus@fjfi.cvut.cz>
2021-11-01 21:07:06 +0300

    debug

--- src/apps/masters/forms.py
+++ src/apps/masters/forms.py
@@ -22,7 +22,7 @@ class MasterCreationForm(forms.ModelForm):
 
     def save(self, commit=True):
         """ Save the provided password in hashed format """
-        user = super(UserCreationForm, self).save(commit=False)
+        user = super(MasterCreationForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
